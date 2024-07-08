37d07a89a83169a87aef9f9e7106e7c6673babc4
gustavo.kuno <gustavo.mkuno@gmail.com>
2024-07-08 17:00:36 -0300

    .

--- apps/llma3.sh
+++ apps/llma3.sh
@@ -1,1 +1,5 @@
+# Run LLMA3
+docker run -d -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
+
+# Run OpenApi Web
 docker run -d -p 3000:8080 --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:main
